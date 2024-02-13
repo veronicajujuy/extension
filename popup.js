767abdb4671efe42ee8a48a7b5c1607978364e89
veronicajujuy <veronica.jujuy@gmail.com>
2024-02-13 17:44:30 -0300

    agregando background.js y configuraciones en el manifest

--- popup.js
+++ popup.js
@@ -1,9 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("hace-algo").addEventListener("click", function () {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {
-        action: "tomarAsistencia",
-      });
-    });
+    chrome.tabs.query(
+      {
+        active: true,
+        currentWindow: true,
+      },
+      function (tabs) {
+        console.log("tabs", tabs);
+        if (tabs[0]) {
+          chrome.runtime.sendMessage({
+            action: "tomarAsistencia",
+            tabId: tabs[0].id,
+          });
+        }
+      }
+    );
   });
 });
