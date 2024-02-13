c7124789cf15a424727df87bbb40b3f81929d738
veronicajujuy <veronica.jujuy@gmail.com>
2024-02-13 17:45:41 -0300

    agregando configuracion background que faltaba

--- background.js
+++ background.js
@@ -1,13 +1,3 @@
-// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-//   if (message.action === "tomarAsistencia") {
-//     sendResponse({
-//       received: true,
-//       data: message.data,
-//     });
-//   }
-// });
-
-// ver que onda esto
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   if (message.action === "tomarAsistencia" && message.tabId) {
     chrome.tabs.sendMessage(message.tabId, { action: "tomarAsistencia" });
